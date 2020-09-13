a2230e2952c17b12df5bca2d3801ca225c6c887c
BArman1991 <armanbelikov@gmail.com>
2020-09-13 13:47:04 +0300

    import toast and pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,89 @@
-import React, { Fragment, useState } from 'react';
-import './App.css';
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
+import React, { Fragment, useState, useEffect } from "react";
+import "./App.css";
 
-import CreateTeam from './components/createTeam'
-import EditTeam from './components/EditTeam'
-import ListAllTeams from './components/ListAllTeams'
-import Dashboard from './components/Dashboard'
-import Register from './components/Register'
-import Login from './components/Login'
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function App(){
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
-  const setAuth = boolean => {
-    setIsAuthenticated(boolean)
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+import CreateTeam from "./components/createTeam";
+import EditTeam from "./components/EditTeam";
+import ListAllTeams from "./components/ListAllTeams";
+import Dashboard from "./components/Dashboard";
+import Register from "./components/Register";
+import Login from "./components/Login";
+
+toast.configure();
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean);
+  };
+
+  async function isAuth() {
+    try {
+      const response = await fetch("http://localhost:5000/auth/is-verify", {
+        method: "GET",
+        headers: { token: localStorage.token },
+      });
+
+      const parseResponse = await response.json();
+      parseResponse === true
+        ? setIsAuthenticated(true)
+        : setIsAuthenticated(false);
+    } catch (err) {
+      console.error(err.message);
+    }
   }
+
+  useEffect(() => {
+    isAuth();
+  });
 
   return (
     <Fragment>
       <Router>
-        <div className='container'>
+        <div className="container">
           <Switch>
-            <Route exact path='/login' render={props => !isAuthenticated?(<Login {...props} setAuth={setAuth}/>):(<Redirect to="/dashboard"/>)} />
-            <Route exact path='/register' render={props => !isAuthenticated?(<Register {...props} setAuth={setAuth}/>):(<Redirect to="login"/>)} />
-            <Route exact path='/dashboard' render={props => isAuthenticated?(<Dashboard {...props} setAuth={setAuth}/>):(<Redirect to="login"/>)} />
+            <Route
+              exact
+              path="/login"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Login {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/dashboard" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/register"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Register {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="login" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/dashboard"
+              render={(props) =>
+                isAuthenticated ? (
+                  <Dashboard {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="login" />
+                )
+              }
+            />
           </Switch>
           {/*<CreateTeam />
           <ListAllTeams />*/}
