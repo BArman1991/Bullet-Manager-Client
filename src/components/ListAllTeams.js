57a4caf3e54b73f5d57d5c5768bc6fb9811dfb49
BArman1991 <armanbelikov@gmail.com>
2020-09-13 13:41:33 +0300

    add prettier

--- src/components/ListAllTeams.js
+++ src/components/ListAllTeams.js
@@ -1,52 +1,47 @@
-import React, {Fragment, useEffect, useState} from "react"
-import EditTeam from "./EditTeam"
+import React, { Fragment, useEffect, useState } from "react";
+import EditTeam from "./EditTeam";
 
 const ListAllTeams = () => {
-    const [teams, setTeams] = useState([])
+  const [teams, setTeams] = useState([]);
 
-    
-    
-    const getTeams = async () => {
-        try {
-            const response = await fetch("http://localhost:5000/Teams")
-            const jsonData = await response.json();
+  const getTeams = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/Teams");
+      const jsonData = await response.json();
 
-            setTeams(jsonData)
-        } catch (err) {
-            console.error(err.message);
-        }
-
+      setTeams(jsonData);
+    } catch (err) {
+      console.error(err.message);
     }
-    
-    useEffect(() => {
-        getTeams();
-    }, []);
-    console.log(teams);
-    return (
-        <Fragment>
-            {" "}
-            <table className="table mt-5 text-center">
-                <thead>
-                <tr>
-                    <th>Team Name</th>
-                    <th>Edit</th>
-                </tr>
-                </thead>
-                <tbody>
-                {/*<tr>
-                    <td>John</td>
-                    <td>Doe</td>
-                </tr> */}
-                {teams.map(team => (
-                    <tr key={team.team_id}>
-                        <td>{team.team_name}</td>
-                        <td><EditTeam team={team}/></td>
-                    </tr> 
-                ))}
-                
-                </tbody>
-            </table>
-        </Fragment>)
-}
+  };
+
+  useEffect(() => {
+    getTeams();
+  }, []);
+  console.log(teams);
+  return (
+    <Fragment>
+      {" "}
+      <table className="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Team Name</th>
+            <th>Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teams.map((team) => (
+            <tr key={team.team_id}>
+              <td>{team.team_name}</td>
+              <td>
+                <EditTeam team={team} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
 
 export default ListAllTeams;
